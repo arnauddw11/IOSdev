ac8bf9f01dea35049f2447f36bfe29ad5de54e7c
Arnaud <arnaud.deweghe@student.hogent.be>
2023-09-27 11:02:08 +0200

    First test commit
    new project created

---
+++ Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Arnaud De Weghe on 26/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Arnaud De Weghe on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
